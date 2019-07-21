eca4060bad6f6aad05579c176b2cdb296e4ed5c2
vhetet <vhetet@solstice.com>
2019-07-21 00:07:48 -0500

    update style of table and share button

--- src/ShareButtonComponent/ShareButton.js
+++ src/ShareButtonComponent/ShareButton.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { Button } from 'reactstrap';
+
+import './ShareButton.css';
+
 const shareButton = (props) =>{
     return(
         <Button onClick={props.onClick} color="info">Share Info</Button>
